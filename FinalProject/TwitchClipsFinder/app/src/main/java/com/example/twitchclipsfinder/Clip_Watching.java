c25860d5efd7b1ac61816eb5edf231953c7dded1
MichaelMcGilvray <mikeymcgilvray@gmail.com>
2021-11-24 15:21:11 -0500

    11/24 Progress Report

--- FinalProject/TwitchClipsFinder/app/src/main/java/com/example/twitchclipsfinder/Clip_Watching.java
+++ FinalProject/TwitchClipsFinder/app/src/main/java/com/example/twitchclipsfinder/Clip_Watching.java
@@ -3,6 +3,8 @@ package com.example.twitchclipsfinder;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -10,10 +12,16 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import java.io.BufferedReader;
@@ -34,6 +42,7 @@ public class Clip_Watching extends AppCompatActivity {
     private TextView title;
     private TextView broadcaster;
     private TextView views;
+    private Button fullscreenButton;
     private Button testButton;
 
     @Override
@@ -47,45 +56,19 @@ public class Clip_Watching extends AppCompatActivity {
         testButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*
-                try {
-                    Clip clips[] = new clipGenerator().getClips("743");
-                    for (int i = 0; i < clips.length; i++) {
-                        clips[i].print();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                 */
+                System.out.print("Test button clicked!\n");
+            }
+        });
 
-                // Temporarily, just open the clip in a browser
-                Intent intent = getIntent();
-                Clip clip = (Clip) intent.getSerializableExtra("clip");
-
-                Uri uri = Uri.parse(clip._url);
-                Intent openClipIntent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(openClipIntent);
+        fullscreenButton = findViewById(R.id.fullscreenButton);
+        fullscreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.print("Fullscreen clicked!\n");
             }
         });
 
         setupClip();
-
-        // Setup the video view using embed url
-        VideoView videoView = findViewById(R.id.videoView);
-        String url = "https://clips.twitch.tv/embed?clip=SleepyGracefulChickpeaDBstyle";
-        String url2 = "https://clips.twitch.tv/SleepyGracefulChickpeaDBstyle";
-//        Uri uri = Uri.parse(url);
-//        videoView.setVideoURI(uri);
-
-//        videoView.setVideoPath(url);
-
-//        videoView.start();
     }
 
     public void setupClip() {
@@ -102,5 +85,25 @@ public class Clip_Watching extends AppCompatActivity {
         broadcaster.setText(clip._broadcaster_name);
         // NumberFormat adds the commas to views
         views.setText((NumberFormat.getInstance().format(clip._views)) + " views");
+
+        loadVideo(clip._embed_url);
+
+        clip.print();
+    }
+
+
+    public void loadVideo(String embed_url) {
+        WebView webView = findViewById(R.id.webView);
+
+        // Setup the webView settings
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+
+        // Load the clip using TwitchClipsFinder site I made
+        String TwitchClipsFinderURL = "https://michaelmcgilvray.github.io/Twitch-Clips-Finder/?clipURL=";
+        webView.loadUrl(TwitchClipsFinderURL + embed_url);
+        
+        System.out.print("Video started\n");
     }
 }
